a73f0fafedb2d35ba3e15eb271f68957520a6f43
Wadezkii <weeti.tiirikainen@hotmail.fi>
2023-05-19 00:08:58 +0300

    completed the unicafe assignment

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,51 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-const Statistics = ({ good, neutral, bad, total, avg, positive }) => {
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text} </td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+/* en tiedä onko oikea ratkaisu mainittuun validateDOMNesting ongelmaan, mutta toimii*/
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+  if (total === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td>No reviews yet</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>statistics</h1>
-      <p>
-        good {good} <br />
-        neutral {neutral} <br />
-        bad {bad} <br />
-        total {total} <br />
-        avg {avg} <br />
-        positive {positive}%
-      </p>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="total" value={total} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={`${positive}%`} />
+        </tbody>
+      </table>
     </div>
-  );
-};
+  )
+}
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleButtonClick = (type) => {
-    setTotal(total + 1);
+    setTotal(total + 1)
 
     const updateFunctions = {
       good: setGood,
       neutral: setNeutral,
       bad: setBad,
     }
-    updateFunctions[type](prevValue => prevValue + 1);
+
+    updateFunctions[type]((prevValue) => prevValue + 1)
   }
 
-  const avg = (good - bad) / total || 0
-  const positive = (good / total) * 100 || 0
+  const average = (good - bad) / total || 0
+  const positive = ((good / total) * 100) || 0
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => handleButtonClick('good') + (avg + 1)}>good</button>
-      <button onClick={() => handleButtonClick('neutral') + (avg + 0)}>neutral</button>
-      <button onClick={() => handleButtonClick('bad') + (avg + -1)}>bad</button>
+      <Button text="Good" handleClick={() => handleButtonClick('good')} />
+      <Button text="Neutral" handleClick={() => handleButtonClick('neutral')} />
+      <Button text="Bad" handleClick={() => handleButtonClick('bad')} />
 
-      <Statistics good={good} neutral={neutral} bad={bad} total={total} avg={avg} positive={positive}  />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
     </div>
   );
 };
 
-export default App;
+export default App
