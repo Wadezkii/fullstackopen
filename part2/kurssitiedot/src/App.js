371cb7961308f48bdccce34fe3c0cb603ef11447
Wadezkii <weeti.tiirikainen@hotmail.fi>
2023-05-21 14:20:05 +0300

    2.5 asti done

--- part2/kurssitiedot/src/App.js
+++ part2/kurssitiedot/src/App.js
@@ -1,36 +1,53 @@
 import Course from './components/Course'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-      name: 'adsjoofdsaiijfosdaoijfgasdiojfdsaoidfs',
-      exercises: 193,
-      id: 4
-      }
-    ]
-  }
-
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'afsdhgksdakasdf',
+          exercises: 194,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
   return (
     <div>
-      <Course course={course} />
+      <Course courses={courses} />
     </div>
   )
 }
--- part2/kurssitiedot/src/components/Course.js
+++ part2/kurssitiedot/src/components/Course.js
@@ -1,16 +1,21 @@
-const Course = ({ course }) => {
-    const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0);
+const Course = ({ courses }) => {
     return (
-        <div>
-            <h1>{course.name}</h1>
+      <div>
+        <h1>notta kurssia kerrakseen</h1>
+        {courses.map(course => (
+          <div key={course.id}>
+            <h2>{course.name}</h2>
             {course.parts.map(part => (
-                <div key={part.id}>
-                    <p>{part.name} {part.exercises}</p>
-                </div>
+              <div key={part.id}>
+                <p>{part.name} {part.exercises}</p>
+              </div>
             ))}
-            <b>total: {totalExercises}</b>
-        </div>
+            <b>total: {course.parts.reduce((total, part) => total + part.exercises, 0)}</b>
+          </div>
+        ))}
+      </div>
     )
-}
+  }
+  
     
 export default Course
